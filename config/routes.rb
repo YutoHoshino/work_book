e4a9b7ae7b593455623166a67e5a3936d1b38005
Yuto Hoshino <63275691+YutoHoshino@users.noreply.github.com>
2020-10-02 22:51:43 +0900

    sign_up
    サインアップ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  root to: "homes#index"
+  resource  :user, only:[:new, :create, :show]
 end
