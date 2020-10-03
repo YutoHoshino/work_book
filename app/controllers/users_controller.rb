c2b0aad060751bc4757c2c86cd01889df0ebe721
Yuto Hoshino <63275691+YutoHoshino@users.noreply.github.com>
2020-10-03 21:05:11 +0900

    ‘エラーメッセージ’

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash.now[:alert] = "パスワードが一致しません"
       render :new
     end
   end
