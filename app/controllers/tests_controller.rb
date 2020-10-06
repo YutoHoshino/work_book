687fbc66640f80dd0e6c10fc996b69f1f6594101
Yuto Hoshino <63275691+YutoHoshino@users.noreply.github.com>
2020-10-06 20:27:39 +0900

    Ajax

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -13,16 +13,18 @@ class TestsController < ApplicationController
   end
 
   def create
-    if @answer = (params[:correct_question]).to_i ==  (params[:question]).delete("{:value=>}").to_i
-      respond_to do |format|
-        format.html { redirect_to new_test_path}
-        format.json { render json: @answer}
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to new_test_path }
-        format.json { render json: @answer}
-      end
-    end
+    @questions = Question.all.sample(11)
+    @description = [
+      @questions[0],
+      @questions[1],
+      @questions[2]
+  ]
+
+  @descriptions = @description.sample(3)
+
+  @question = @description[1]
+    @answer = (params[:correct_question]).to_i ==  (params[:question]).delete("{:value=>}").to_i
+
+
   end
 end
--- app/javascript/packs/question.js
+++ app/javascript/packs/question.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-  $(function () {
+// document.addEventListener('DOMContentLoaded', () => {
+//   $(function () {
 
-    $('.js-form').on('submit', function(e) {
-      e.preventDefault();
+//     $('.js-form').on('submit', function(e) {
+//       e.preventDefault();
 
 
-      //#変数に答えのIDと問題のIDを取得
-      let answer = $("input[name='question']:checked").val().replace(/[^0-9]/g, '');
-      let question = $('.hidden').val();
+//       //#変数に答えのIDと問題のIDを取得
+//       let answer = $("input[name='question']:checked").val().replace(/[^0-9]/g, '');
+//       let question = $('.hidden').val();
 
 
       
 
 
-      $.ajax({
-        type: 'GET',
-        url: '/tests/new',
-        data: {
+//       $.ajax({
+//         type: 'GET',
+//         url: '/tests/new',
+//         data: {
 
-        },
-        datatType: 'json'
-      })
-      .done(function() {
+//         },
+//         datatType: 'json'
+//       })
+//       .done(function() {
 
 
 
-        $('.form__submit').attr('disabled', false);
-        alert('sucsess');
-      })
-      .fail(function() {
-        alert('error');
-      });
-    });
-  });
-});
+//         $('.form__submit').attr('disabled', false);
+//         alert('sucsess');
+//       })
+//       .fail(function() {
+//         alert('error');
+//       });
+//     });
+//   });
+// });
