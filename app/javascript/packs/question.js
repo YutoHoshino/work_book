b3049a0368371a826cb1aa9a49bff060b8666e85
Yuto Hoshino <63275691+YutoHoshino@users.noreply.github.com>
2020-10-06 17:17:59 +0900

    disabled

--- app/javascript/packs/question.js
+++ app/javascript/packs/question.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   $(function () {
+
     $('.js-form').on('submit', function(e) {
       e.preventDefault();
+
+
+      //#変数に答えのIDと問題のIDを取得
       let answer = $("input[name='question']:checked").val().replace(/[^0-9]/g, '');
       let question = $('.hidden').val();
 
-      console.log(answer)
-      console.log(question)
+
+      
+
 
       $.ajax({
         type: 'GET',
         url: '/tests/new',
+        data: {
+
+        },
         datatType: 'json'
       })
       .done(function() {
+
+
+
+        $('.form__submit').attr('disabled', false);
         alert('sucsess');
       })
       .fail(function() {
