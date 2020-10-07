839248a9edbbb888fbbf824ca178aa1f74541b8d
Yuto Hoshino <63275691+YutoHoshino@users.noreply.github.com>
2020-10-07 10:59:07 +0900

    問題のsession

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,30 +1,43 @@
 class TestsController < ApplicationController
   def new
+    # 問題の変数
     @questions = Question.all.sample(11)
-    @description = [
-      @questions[0],
-      @questions[1],
-      @questions[2]
-  ]
+    @description = [@questions[0],@questions[1],@questions[2]]
+    @descriptions = @description.sample(3)
+    @question = @description[1]
 
-  @descriptions = @description.sample(3)
-
-  @question = @description[1]
+    # sessionの初期変数
+    session[:correct_answer] = 0
+    session[:incorrect_answer] = 0
+    session[:answer_check] = ''
+    session[:answer_rate] = 1
   end
 
   def create
+    # 問題の変数
     @questions = Question.all.sample(11)
-    @description = [
-      @questions[0],
-      @questions[1],
-      @questions[2]
-  ]
+    @description = [@questions[0],@questions[1],@questions[2]]
+    @descriptions = @description.sample(3)
+    @question = @description[1]
 
-  @descriptions = @description.sample(3)
 
-  @question = @description[1]
-    @answer = (params[:correct_question]).to_i ==  (params[:question]).delete("{:value=>}").to_i
+    # 問題のIDと解答のID(params)
+    params_correct = (params[:correct_question]).delete("{:value=>}").to_i
+    params_question = (params[:question]).delete("{:value=>}").to_i
 
+
+    # 問題ページを遷移することに+1
+    session[:answer_rate] +=1
+
+
+  # 答え合わせの条件分岐
+    if params_question == params_correct
+      session[:answer_check] = '正解'
+      session[:correct_answer] +=1
+    else
+      session[:answer_check] = '不正解'
+      session[:incorrect_answer] +=1
+    end
 
   end
 end
